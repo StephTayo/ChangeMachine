6c135c344be7d67836334b2ff9446ab7aae1827c
Stephen Omotayo <127545231+StephTayo@users.noreply.github.com>
2023-05-11 15:25:39 -0600

    Add files via upload

---
+++ Change Machine - No Penny/Change Machine/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Change_Machine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMoney = new System.Windows.Forms.TextBox();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTwoonies = new System.Windows.Forms.Label();
+            this.lblLoonies = new System.Windows.Forms.Label();
+            this.lblQuarters = new System.Windows.Forms.Label();
+            this.lblDimes = new System.Windows.Forms.Label();
+            this.lblNickels = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblCents = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "$:";
+            // 
+            // txtMoney
+            // 
+            this.txtMoney.Location = new System.Drawing.Point(137, 20);
+            this.txtMoney.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMoney.Name = "txtMoney";
+            this.txtMoney.Size = new System.Drawing.Size(118, 26);
+            this.txtMoney.TabIndex = 1;
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(157, 67);
+            this.btnChange.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(98, 34);
+            this.btnChange.TabIndex = 2;
+            this.btnChange.Text = "&Change";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Twoonie: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Loonie: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(95, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Quarter: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(95, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Dime: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(95, 336);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Nickel: ";
+            // 
+            // lblTwoonies
+            // 
+            this.lblTwoonies.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTwoonies.Location = new System.Drawing.Point(189, 171);
+            this.lblTwoonies.Name = "lblTwoonies";
+            this.lblTwoonies.Size = new System.Drawing.Size(66, 25);
+            this.lblTwoonies.TabIndex = 9;
+            this.lblTwoonies.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLoonies
+            // 
+            this.lblLoonies.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblLoonies.Location = new System.Drawing.Point(189, 212);
+            this.lblLoonies.Name = "lblLoonies";
+            this.lblLoonies.Size = new System.Drawing.Size(66, 25);
+            this.lblLoonies.TabIndex = 10;
+            this.lblLoonies.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblQuarters
+            // 
+            this.lblQuarters.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblQuarters.Location = new System.Drawing.Point(189, 254);
+            this.lblQuarters.Name = "lblQuarters";
+            this.lblQuarters.Size = new System.Drawing.Size(66, 25);
+            this.lblQuarters.TabIndex = 11;
+            this.lblQuarters.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDimes
+            // 
+            this.lblDimes.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDimes.Location = new System.Drawing.Point(189, 293);
+            this.lblDimes.Name = "lblDimes";
+            this.lblDimes.Size = new System.Drawing.Size(66, 25);
+            this.lblDimes.TabIndex = 12;
+            this.lblDimes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNickels
+            // 
+            this.lblNickels.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNickels.Location = new System.Drawing.Point(189, 331);
+            this.lblNickels.Name = "lblNickels";
+            this.lblNickels.Size = new System.Drawing.Size(66, 25);
+            this.lblNickels.TabIndex = 13;
+            this.lblNickels.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(41, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(125, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Rounded Cents:";
+            // 
+            // lblCents
+            // 
+            this.lblCents.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCents.Location = new System.Drawing.Point(189, 116);
+            this.lblCents.Name = "lblCents";
+            this.lblCents.Size = new System.Drawing.Size(66, 25);
+            this.lblCents.TabIndex = 15;
+            this.lblCents.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(335, 391);
+            this.Controls.Add(this.lblCents);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblNickels);
+            this.Controls.Add(this.lblDimes);
+            this.Controls.Add(this.lblQuarters);
+            this.Controls.Add(this.lblLoonies);
+            this.Controls.Add(this.lblTwoonies);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.txtMoney);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMoney;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTwoonies;
+        private System.Windows.Forms.Label lblLoonies;
+        private System.Windows.Forms.Label lblQuarters;
+        private System.Windows.Forms.Label lblDimes;
+        private System.Windows.Forms.Label lblNickels;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblCents;
+    }
+}
+
---
+++ Change Machine - No Penny/Change Machine/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Change_Machine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // gives change using the smallest number of coins (assumes no penny coins)
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            int nrTwoonies, nrLoonies, nrQuarters, nrDimes, nrNickels;
+
+            double money; // input $ amount as double
+            int cents; // money amount in cents
+            
+            // get  money in dollars and convert to cents
+            money = Convert.ToDouble(txtMoney.Text);
+            //cents = (int)(money * 100); // could lose one cent.Try 2.03
+            cents = (int)Math.Round(money * 100);
+
+            // round cents to the closest multiple of 5
+            // do deal with the lack of one penny coins
+            switch(cents %5 )
+            {
+                case 1:
+                    cents -= 1;
+                    break;
+                case 2:
+                    cents -= 2;
+                    break;
+                case 3:
+                    cents += 2;
+                    break;
+                case 4:
+                    cents += 1;
+                    break;
+            }
+            lblCents.Text = cents.ToString();
+
+            //give change
+            nrTwoonies = cents / 200;
+            cents = cents % 200;
+            nrLoonies = cents / 100;
+            cents = cents % 100;
+            nrQuarters = cents / 25;
+            cents = cents % 25;
+            nrDimes = cents / 10;
+            cents = cents % 10;
+            nrNickels = cents / 5;
+
+            // display results
+            lblTwoonies.Text = nrTwoonies.ToString();
+            lblLoonies.Text = nrLoonies.ToString();
+            lblQuarters.Text = nrQuarters.ToString();
+            lblDimes.Text = nrDimes.ToString();
+            lblNickels.Text = nrNickels.ToString();
+        }
+    }
+}
